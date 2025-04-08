818b506c9d4692a10984ca8739977abbf879c71c
maju <71796144+majusalins@users.noreply.github.com>
2025-04-07 21:11:10 -0400

    Aula do dia 07/04/2025

---
+++ AppPessoa/AppPessoa/Contexto/ConextBD.cs
@@ -0,0 +1,16 @@
+﻿using AppPessoa.Models;
+using AppPessoa.Services;
+
+namespace AppPessoa.Contexto
+{
+    public class ContextBD
+    {
+        public ContextBD()
+        {
+            Pessoas = new List<Pessoa>();
+        }    
+        public List<Pessoa>? Pessoas { get; set; }
+
+    }
+
+}
---
+++ AppPessoa/AppPessoa/Services/PessoaService.cs
@@ -0,0 +1,38 @@
+﻿using AppPessoa.Contexto;
+using AppPessoa.Models;
+
+namespace AppPessoa.Services
+{
+    public class PessoaService
+    {
+        private readonly ContextBD _context;
+
+        public PessoaService(ContextBD con)
+        {
+            _context = con;
+        }
+
+        public async Task<List<Pessoa>>? Pessoas()
+        {
+            var pessoas = _context.Pessoas?.ToList();
+            return pessoas;
+        }
+
+        public async Task<Pessoa>? GetPessoa(int id)
+        {
+            var pessoa = _context.Pessoas?.Where(p => p.Id == id).FirstOrDefault();
+            return pessoa;
+        } 
+        
+        public async Task<Pessoa>? GetPessoa(string cpf)
+        {
+            var pessoa = _context.Pessoas?.Where(p => p.Cpf == cpf).FirstOrDefault();
+            return pessoa;
+        }
+
+        public async Task Add(Pessoa pessoa)
+        {
+            _context.Pessoas?.Add(pessoa);
+        }
+    }
+}
